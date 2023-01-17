bc383567fdd85ca671d129757b7f988875471230
Ivan Chernyakov <3699818@mail.ru>
2023-01-17 17:20:22 +0300

    Add animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import {SelectModule} from './select/select.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SelectModule
+    SelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,9 +1,18 @@
-import {Component, HostListener, Input} from '@angular/core';
+import {animate, AnimationEvent, state, style, transition, trigger} from '@angular/animations';
+import {Component, EventEmitter, HostListener, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
+  animations: [
+    trigger('dropDown', [
+      state('void', style({transform: 'scaleY(0)', opacity: 0})),
+      state('*', style({transform: 'scaleY(1)', opacity: 1})),
+      transition(':enter', [animate('320ms cubic-bezier(0, 1, 0.45, 1.34)')]),
+      transition(':leave', [animate('420ms cubic-bezier(0.88, -0.7, 0.86, 0.85)')])
+    ])
+  ]
 })
 export class SelectComponent {
 
@@ -12,6 +21,12 @@ export class SelectComponent {
 
   @Input()
   value: string | null = ''
+
+  @Output()
+  readonly opened = new EventEmitter()
+
+  @Output()
+  readonly closed = new EventEmitter()
 
   @HostListener('click')
   open() {
@@ -22,6 +37,15 @@ export class SelectComponent {
 
   close() {
     this.isOpen = false
+  }
+
+  onPanelAnimationDone({ fromState, toState }: AnimationEvent) {
+    if (fromState === 'void' && toState === null && this.isOpen) {
+      this.opened.emit();
+    }
+    if (fromState === null && toState === 'void' && !this.isOpen) {
+      this.closed.emit();
+    }
   }
 
 }
