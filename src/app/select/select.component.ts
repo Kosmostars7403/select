4f8aa156480907c02be4ac1d4c8e9005c4a106bf
Ivan Chernyakov <3699818@mail.ru>
2023-01-17 16:57:10 +0300

    Add  layout

---
+++ src/app/select/select.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss']
+})
+export class SelectComponent {
+
+  @Input()
+  label = '';
+
+  value = ''
+
+}
